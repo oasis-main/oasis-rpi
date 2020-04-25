a9c1eedb1417b77210d4c9fa0b9edf9f97d6337b
Mike <leemichael289@gmail.com>
2020-04-24 20:53:33 -0400

    added lux to sensor array

--- sensorArray/sensorArray.ino
+++ sensorArray/sensorArray.ino
@@ -1,5 +1,4 @@
-// Example testing sketch for various DHT humidity/temperature sensors
-// Written by ladyada, public domain
+// Sensor Sketch for Oasis Grow Device
 
 // REQUIRES the following Arduino libraries:
 // - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
@@ -7,7 +6,7 @@
 
 #include "DHT.h"
 
-#define DHTPIN 3     // Digital pin connected to the DHT sensor
+#define DHTPIN 2     // Digital pin connected to the DHT sensor
 // Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
 // Pin 15 can work but DHT must be disconnected during program upload.
 
@@ -15,7 +14,7 @@
 //#define DHTTYPE DHT11   // DHT 11
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
-
+//DHT22 setup:
 // Connect pin 1 (on the left) of the sensor to +5V
 // NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
 // to 3.3V instead of 5V!
@@ -23,35 +22,49 @@
 // Connect pin 4 (on the right) of the sensor to GROUND
 // Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
 
-// Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
+// Initialize DHT sensor (temp & hum)
 DHT dht(DHTPIN, DHTTYPE);
 
-void setup() {
-  Serial.begin(9600);
-  //Serial.println(F("DHTxx test!"));
+//Initialize GY-30 sensor (lux)
+#include <Wire.h>
+int BH1750_address = 0x23; // i2c Addresses
+byte buff[2];
 
-  dht.begin();
+void setup() {
+  Wire.begin(); //start wire
+  BH1750_Init(BH1750_address); //start GY-30 on i2c
+  delay(200); //wait so nothing weird happens
+  Serial.begin(9600); //start the serial monitor
+  dht.begin(); //start DHT
+  
 }
 
 void loop() {
   // Wait a few seconds between measurements.
   delay(1000);
 
+  //DHT-22
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
+  //float t = dht.readTemperature();
   // Read temperature as Fahrenheit (isFahrenheit = true)
   float f = dht.readTemperature(true);
 
+  //GY-30
+  float valf=0;
+  float l = 0;
+  if(BH1750_Read(BH1750_address)==2){
+    
+    valf=((buff[0]<<8)|buff[1])/1.2;
+    
+    if(valf<0) l = -1;
+    else l = (int)valf; 
+  }
 
-  //tested Nan
-  //h=sqrt(-1);
 
+  //define edge cases for faulty readings
   if (isnan(h)){
     h=-1;
   }
@@ -59,14 +72,30 @@ void loop() {
     f=-1;
   }
 
-
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-
   Serial.print(h);
   Serial.print(' ');
-  Serial.println(f);
+  Serial.print(f);
+  Serial.print(' ');
+  Serial.println(l);
 
+}
+
+void BH1750_Init(int address){
+  
+  Wire.beginTransmission(address);
+  Wire.write(0x10); // 1 [lux] aufloesung
+  Wire.endTransmission();
+}
+
+byte BH1750_Read(int address){
+  
+  byte i=0;
+  Wire.beginTransmission(address);
+  Wire.requestFrom(address, 2);
+  while(Wire.available()){
+    buff[i] = Wire.read(); 
+    i++;
+  }
+  Wire.endTransmission();  
+  return i;
 }
