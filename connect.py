d7515749e59b04a75267ebcefd7a09fa466f1a14
Mike <mike@oasisregenerative.com>
2020-11-09 16:15:43 -0500

    changed server static IP

--- connect.py
+++ connect.py
@@ -2,37 +2,26 @@
 import os
 import time
 
-#WiFi down, AP up
-os.system("sudo ifconfig wlan0 down")
-time.sleep(1)
-os.system("sudo systemctl stop hostapd")
-time.sleep(1)
-os.system("sudo systemctl stop dhcpcd")
-time.sleep(1)
-os.system("sudo cp /etc/dhcpcd_AP.conf /etc/dhcpcd.conf")
-time.sleep(1)
-os.system("sudo cp /etc/dnsmasq_AP.conf /etc/dnsmasq.conf")
-time.sleep(1)
-os.system("sudo systemctl daemon-reload")
-time.sleep(1)
-os.system("sudo rfkill unblock wlan")
-time.sleep(1)
-os.system("sudo systemctl unmask hostapd")
-time.sleep(1)
-os.system("sudo systemctl start hostapd")
-time.sleep(1)
-os.system("sudo systemctl restart dhcpcd")
-time.sleep(1)
-os.system("sudo ifconfig wlan0 up")
+def WiFidownAPup():
+    os.system("sudo ifconfig wlan0 down")
+    os.system("sudo systemctl stop hostapd")
+    os.system("sudo systemctl stop dhcpcd")
+    os.system("sudo cp /etc/dhcpcd_AP.conf /etc/dhcpcd.conf")
+    os.system("sudo cp /etc/dnsmasq_AP.conf /etc/dnsmasq.conf")
+    os.system("sudo systemctl daemon-reload")
+    os.system("sudo rfkill unblock wlan")
+    os.system("sudo systemctl unmask hostapd")
+    os.system("sudo systemctl start hostapd")
+    os.system("sudo systemctl restart dhcpcd")
+    os.system("sudo ifconfig wlan0 up")
 
-#keep AP up for 75 seconds (15 to join the network, 60 to connect)
-time.sleep(75)
+def APdownWiFiup():
+    os.system("sudo ifconfig wlan0 down")
+    os.system("sudo cp /etc/dhcpcd_WiFi.conf /etc/dhcpcd.conf")
+    os.system("sudo cp /etc/dnsmasq_WiFi.conf /etc/dnsmasq.conf")
+    os.system("sudo systemctl daemon-reload")
+    os.system("sudo systemctl restart dhcpcd")
+    os.system("sudo ifconfig wlan0 up")
 
-#AP down, WiFi up
-os.system("sudo ifconfig wlan0 down")
-os.system("sudo cp /etc/dhcpcd_WiFi.conf /etc/dhcpcd.conf")
-os.system("sudo cp /etc/dnsmasq_WiFi.conf /etc/dnsmasq.conf")
-os.system("sudo systemctl daemon-reload")
-os.system("sudo systemctl restart dhcpcd")
-os.system("sudo ifconfig wlan0 up")
-
+#WiFidownAPup()
+APdownWiFiup()
