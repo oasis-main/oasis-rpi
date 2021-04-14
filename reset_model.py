d07e3603c5192d28f5e797a4b382fb4ab71aa5aa
Oasis <71035180+oasis-iot@users.noreply.github.com>
2021-04-14 19:37:36 -0400

    Added default reset config

--- reset_model.py
+++ reset_model.py
@@ -1,41 +1,36 @@
 import json
+from subprocess import Popen
 
-with open('/home/pi/device_state.json', 'r+') as d:
-                        data = json.load(d)
-                        data['connected'] = "0" # <--- reset to 0
-                        data['running'] = "0"
-                        data['LEDstatus'] = "off"
-                        data['LEDtimeon'] = "0"
-                        data['LEDtimeoff'] = "0"
-                        data['AccessPoint'] = "0"
-                        data['awaiting_update'] = "0"
-                        d.seek(0) # <--- should reset file position to the beginning.
-                        json.dump(data, d)
-                        d.truncate() # remove remaining part
-d.close()
+def reset_device_state():
+    reset_process = Popen(["sudo", "cp", "/home/pi/grow-ctrl/device_state_default_template.json", "/home/pi/device_state.json"])
+    reset_process.wait()
+    reset_process = Popen(["sudo","cp","/home/pi/grow-ctrl/device_state_default_template.json", "/home/pi/device_state_buffer.json"])
+    reset_process.wait()
+  
+def reset_grow_params():
+    reset_process = Popen(["sudo", "cp", "/home/pi/grow-ctrl/grow_params_default_template.json", "/home/pi/grow_params.json"])
+    reset_process.wait()
+    reset_process = Popen(["sudo","cp","/home/pi/grow-ctrl/grow_params_default_template.json", "/home/pi/grow_params_buffer.json"])
+    reset_process.wait()
+  
+def reset_access_config():
+    reset_process = Popen(["sudo", "cp", "/home/pi/grow-ctrl/access_config_default_template.json", "/home/pi/access_config.json"])
+    reset_process.wait()
+    reset_process = Popen(["sudo","cp","/home/pi/grow-ctrl/access_config_default_template.json", "/home/pi/access_config_buffer.json"])
+    reset_process.wait()
+  
+def reset_hardware_config():
+    reset_process = Popen(["sudo", "cp", "/home/pi/grow-ctrl/hardware_config_default_template.json", "/home/pi/hardware_config.json"])
+    reset_process.wait()
+  
+def reset_feature_toggles()
+    reset_process = Popen(["sudo", "cp", "/home/pi/grow-ctrl/feature_toggles_default_template.json", "/home/pi/feature_toggles.json"])
+    reset_process.wait()
+  
+def reset_logs()
+    reset_process = Popen(["sudo", "cp", "/home/pi/grow-ctrl/growCtrl_log_default_template.json", "/home/pi/logs/growCtrl_log.json"])
+    reset_process.wait()
 
-with open('/home/pi/logs/growCtrl_log.json', 'r+') as l:
-                        data = json.load(l)
-                        data['last_start_mode'] = "fresh, no run history" # <--- reset to
-                        l.seek(0) # <--- should reset file position to the beginning.
-                        json.dump(data, l)
-                        l.truncate() # remove remaining part
-l.close()
-
-with open('/home/pi/grow_params.json', 'r+') as g:
-                        data = json.load(g)
-                        data["targetT"] = "70"
-                        data["targetH"] = "90"
-                        data["targetL"] = "on"
-                        data["LtimeOn"] = "8"
-                        data["LtimeOff"] = "22"
-                        data["lightInterval"] = "60"
-                        data["cameraInterval"] = "3600"
-                        data["waterMode"] = "off"
-                        data["waterDuration"] = "60"
-                        data["waterInterval"] =  "3600"
-                        g.seek(0) # <--- should reset file position to the beginning.
-                        json.dump(data, g)
-                        g.truncate() # remove remaining part
-g.close()
-
+  
+if __name__ == "__main__":
+  reset_device_state()
