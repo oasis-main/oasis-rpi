d0c78478c35f11a62a5b01a72aae111bc79eb9a9
Owen Leonard <owenleonard11@gmail.com>
2021-06-10 15:59:35 -0400

    Remove git from env setup (I am an idiot)

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,10 +1,3 @@
-echo "Removing unnecessary packages..."
-sudo apt-get purge --auto-remove gvfs-backends gvfs-fuse
-
-echo "Cloning repositories..."
-cd ~
-git clone https://github.com/oasis-gardens/oasis-grow.git
-cd oasis-grow
-
 echo "Adding packages..."
-pip3 install firebase pyrebase python_jwt gcloud sseclient requests-toolbelt pickle5 pandas pyserial Pillow
+cd oasis-grow
+pip3 install firebase pyrebase python_jwt gcloud sseclient requests-toolbelt pickle5 pandas pyserial Pillowg
