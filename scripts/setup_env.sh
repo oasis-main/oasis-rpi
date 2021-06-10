bd814813cef3f86b3ffd3a7522db805be6d52b95
Owen Leonard <owenleonard11@gmail.com>
2021-06-10 16:20:26 -0400

    Fix package typo

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,2 +1,2 @@
 echo "Adding packages..."
-pip3 install firebase pyrebase python_jwt gcloud sseclient requests-toolbelt pickle5 pandas pyserial Pillowg
+pip3 install firebase pyrebase python_jwt gcloud sseclient requests-toolbelt pickle5 pandas pyserial Pillow
