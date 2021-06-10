34c3652e57ff138e37ac50ae4971b4a634278147
Owen Leonard <owenleonard11@gmail.com>
2021-06-10 16:26:28 -0400

    Use tee in network setup

--- scripts/setup_network.sh
+++ scripts/setup_network.sh
@@ -13,11 +13,12 @@ sudo cp /etc/dnsmasq.conf /etc/dnsmasq_backup.conf
 sudo cp /etc/network/interfaces /etc/network/interfaces_backup
 
 echo "Writing configurations..."
-sudo printf "
+printf "
 interface wlan0
 static ip_address=192.168.4.1/24
-nohook wpa_supplicant" >> /etc/dhcpcd.conf
-sudo printf "
+nohook wpa_supplicant
+" | sudo tee -a /etc/dhcpcd.conf
+printf "
 Listening interface
 interface=wlan0
 #Pool of IP addresses served via DHCP
@@ -25,8 +26,9 @@ dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h
 #Local wireless DNS domain
 domain=wlan
 #Alias for this router
-address=/gw.wlan/192.168.4.1" >> /etc/dnsmasq.conf
-sudo printf "
+address=/gw.wlan/192.168.4.1
+" | sudo tee -a /etc/dnsmasq.conf
+printf "
 country_code=US
 interface=wlan0
 ssid=Oasis
@@ -40,7 +42,7 @@ wpa_passphrase=AardvarkBadgerHedgehog
 wpa_key_mgmt=WPA-PSK
 wpa_pairwise=TKIP
 rsn_pairwise=CCMP
-" >> /etc/hostapd/hostapd.conf
+" | sudo tee -a /etc/hostapd/hostapd.conf
 
 echo "Staging config files for network mode switching..."
 sudo cp /etc/dhcpcd.conf /etc/dhcpcd_AP.conf
