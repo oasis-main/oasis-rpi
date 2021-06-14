71bd39c5526374370b3285ba3dbf9853ccd1528d
Owen Leonard <owenleonard11@gmail.com>
2021-06-14 12:28:27 -0400

    Update PATH in env setup

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -4,4 +4,8 @@ sudo apt-get install -y libopenjp2-7
 sudo apt-get install -y libtiff5
 sudo apt-get install -y libatlas-base-dev
 
+echo "Configuring PATH variable..."
+echo "export PATH=\"home/pi/.local/bin:$PATH" | sudo tee -a /home/pi/.bashrc
+
+echo "Installing modules..."
 pip3 install firebase pyrebase python_jwt gcloud sseclient requests-toolbelt pickle5 pandas pyserial Pillow
