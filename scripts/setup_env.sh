65e4486a3e22f3e988824504d9465b23e44d4534
Owen Leonard <owenleonard11@gmail.com>
2021-06-14 09:26:04 -0400

    Add packages to env setup

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,2 +1,7 @@
 echo "Adding packages..."
+sudo apt install -y python3-pip
+sudo apt-get install -y libopenjp2-7
+sudo apt-get install -y libtiff5
+sudo apt-get install -y libatlas-base-dev
+
 pip3 install firebase pyrebase python_jwt gcloud sseclient requests-toolbelt pickle5 pandas pyserial Pillow
