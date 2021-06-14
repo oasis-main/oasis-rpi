379c0cd1d95142f5788e84b3a3b792bdfb52d446
Owen Leonard <owenleonard11@gmail.com>
2021-06-14 09:40:17 -0400

    Create log directories in config setup

--- scripts/setup_config.sh
+++ scripts/setup_config.sh
@@ -1,3 +1,9 @@
+echo "Creating data directories..."
+mkdir /home/pi/logs
+mkdir /home/pi/data_output
+mkdir /home/pi/data_output/image_feed
+mkdir /home/pi/data_output/sensor_feed
+
 echo "Moving configuration files..."
 cp /oasis-grow/hardware_config_default_template.json /home/pi/hardware_config.json
 cp /oasis-grow/access_config_default_template.json /home/pi/access_config.json
@@ -6,5 +12,4 @@ cp /oasis-grow/device_state_default_template.json /home/pi/device_state_buffer.j
 cp /oasis-grow/grow_params_default_template.json /home/pi/grow_params.json
 cp /oasis-grow/grow_params_default_template.json /home/pi/grow_params_buffer.json
 cp /oasis-grow/feature_toggles_default_template.json /home/pi/feature_toggles.json
-mkdir /home/pi/logs
 cp /oasis-grow/growCtrl_log_default_template.json /home/pi/logs/growCtrl_log.json
