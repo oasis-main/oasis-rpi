b0c44c0b637dc1d89a48f19e1b1a939627426d64
owenleonard11 <50707468+owenleonard11@users.noreply.github.com>
2021-06-17 15:47:24 -0400

    Create venv in environment setup

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -4,8 +4,13 @@ sudo apt-get install -y libopenjp2-7
 sudo apt-get install -y libtiff5
 sudo apt-get install -y libatlas-base-dev
 
+echo "Creating virtual environment..."
+python3 venv -m /home/pi/oasis-grow
+source /home/pi/oasis-grow/bin/activate
+
 echo "Configuring PATH variable..."
 echo "export PATH=\"home/pi/.local/bin:$PATH" | sudo tee -a /home/pi/.bashrc
+source /home/pi/.bashrc
 
 echo "Installing modules..."
 pip3 install firebase pyrebase python_jwt gcloud sseclient requests-toolbelt pickle5 pandas pyserial Pillow
