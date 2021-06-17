19f6ded703751db7e7d4fcaafe20137344551202
owenleonard11 <50707468+owenleonard11@users.noreply.github.com>
2021-06-17 16:43:53 -0400

    Use requirements.txt with pip in env setup

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,11 +1,11 @@
 echo "Adding packages..."
 sudo apt install -y python3-pip
+sudo apt-get install -python3-venv
 sudo apt-get install -y libopenjp2-7
 sudo apt-get install -y libtiff5
 sudo apt-get install -y libatlas-base-dev
 
 echo "Creating virtual environment..."
-pip3 install virtualenv
 python3 venv -m /home/pi/oasis-grow
 source /home/pi/oasis-grow/bin/activate
 
@@ -14,4 +14,4 @@ echo "export PATH=\"home/pi/.local/bin:$PATH" | sudo tee -a /home/pi/.bashrc
 source /home/pi/.bashrc
 
 echo "Installing modules..."
-pip3 install firebase pyrebase python_jwt gcloud sseclient requests-toolbelt pickle5 pandas pyserial Pillow
+pip3 install -r /home/pi/oasis-grow/requirements.txt
