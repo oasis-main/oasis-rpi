ba8e19139f6b3573a6559bf3622e92c3b6481cce
Owen Leonard <owenleonard11@gmail.com>
2021-06-29 15:24:08 -0400

    Run sed as su

--- scripts/setup_bootloader.sh
+++ scripts/setup_bootloader.sh
@@ -1,2 +1,2 @@
 sudo chmod +w /etc/rc.local
-sed -ie "/^fi/a /home/pi/oasis-grow/bin/activate\npython3 controller.py &" /etc/rc.local
+sudo sed -ie "/^fi/a /home/pi/oasis-grow/bin/activate\npython3 controller.py &" /etc/rc.local
