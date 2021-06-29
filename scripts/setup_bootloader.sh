d510f1698e092b01fb962a910f178bbe66c9fce8
Owen Leonard <owenleonard11@gmail.com>
2021-06-29 14:52:09 -0400

    Search for fi in sed edit

--- scripts/setup_bootloader.sh
+++ scripts/setup_bootloader.sh
@@ -1,2 +1,2 @@
 sudo chmod +w /etc/rc.local
-sed -ie "/^/a /home/pi/oasis-grow/bin/activate\npython3 controller.py &" /etc/rc.local
+sed -ie "/^fi/a /home/pi/oasis-grow/bin/activate\npython3 controller.py &" /etc/rc.local
