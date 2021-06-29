54060ad8956dbcb183c2132c00636860b0b9f391
Owen Leonard <owenleonard11@gmail.com>
2021-06-29 15:29:15 -0400

    Add full path to controller in bootloader

--- scripts/setup_bootloader.sh
+++ scripts/setup_bootloader.sh
@@ -1,2 +1,2 @@
 sudo chmod +w /etc/rc.local
-sudo sed -ie "/^fi/a /home/pi/oasis-grow/bin/activate\npython3 controller.py &" /etc/rc.local
+sudo sed -ie "/^fi/a /home/pi/oasis-grow/bin/activate\npython3 /home/pi/oasis-growcontroller.py &" /etc/rc.local
