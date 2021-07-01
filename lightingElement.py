4bd9230ef28a92c1a6bc9fa4ffe4c5c8ace6fffb
Oasis <71035180+oasis-gardens@users.noreply.github.com>
2021-07-01 19:55:33 -0400

    Update lightingElement.py

--- lightingElement.py
+++ lightingElement.py
@@ -12,13 +12,6 @@ import os.path
 import sys
 
 #set proper path for modules
-sys.path.append('/home/pi/oasis-grow')
-sys.path.append('/usr/lib/python37.zip')
-sys.path.append('/usr/lib/python3.7')
-sys.path.append('/usr/lib/python3.7/lib-dynload')
-sys.path.append('/home/pi/.local/lib/python3.7/site-packages')
-sys.path.append('/usr/local/lib/python3.7/dist-packages')
-sys.path.append('/usr/lib/python3/dist-packages')
 
 #import libraries
 import RPi.GPIO as GPIO
