d6386a71851ea2f9c08af58c6c3a8111039b8b87
Oasis <71035180+oasis-gardens@users.noreply.github.com>
2021-07-01 19:32:20 -0400

    Update setup_env.sh

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "This setup is built for Raspbian-Buster Lite"
+
 echo "Adding OS packages..."
 sudo apt-get update
 sudo apt install -y wget software-properties-common build-essential libnss3-dev zlib1g-dev libgdbm-dev libncurses5-dev libssl-dev libffi-dev libreadline-dev libsqlite3-dev libbz2-dev
@@ -8,17 +10,16 @@ sudo apt-get install -y libtiff5
 sudo apt-get install -y libatlas-base-dev
 
 echo "Building Python Environment (3.9.2)..."
-cd /home/pi
 wget https://www.python.org/ftp/python/3.9.2/Python-3.9.2.tgz
 tar xvf Python-3.9.2.tgz
 cd Python-3.9.2/
-./configure --enable-optimizations --enable-shared
+./configure --enable-optimizations
 sudo make altinstall
 python3.9 -V
 
 echo "Pointing all python3 to python3.9..."
-alias python3 = python3.9
-alias pip3 = pip3.9
+alias python3=python3.9
+alias pip3=pip3.9
 echo "alias python3=python3.9" | sudo tee -a /home/pi/.bashrc
 source /home/pi/.bashrc
 echo "alias pip3=pip3.9" | sudo tee -a /home/pi/.bashrc
@@ -29,6 +30,6 @@ echo "export PATH=/home/pi/.local/bin:$PATH/" | sudo tee -a /home/pi/.bashrc
 source /home/pi/.bashrc
 
 echo "Installing modules..."
-sudo pip3 install -r /home/pi/oasis-grow/requirements.txt
+pip3 install -r /home/pi/oasis-grow/requirements.txt
 
 
