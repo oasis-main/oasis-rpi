eaef325873d28f43ac80f6e789171169e24ec81f
Oasis <71035180+oasis-gardens@users.noreply.github.com>
2021-07-03 13:20:23 -0400

    Update send_image_test.py

--- send_image_test.py
+++ send_image_test.py
@@ -1,5 +1,8 @@
 import cameraElement as cam
 
+#set proper path for modules
+sys.path.append("home/pi/.local/lib/python3.9/site-packages")
+
 cam.load_state()
 user, db, storage = cam.initialize_user(cam.access_config["refresh_token"])
 cam.send_image(user, storage, "/home/pi/image.jpg")
