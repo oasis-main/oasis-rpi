db96d4152099a6f97f710620b8c70afea14fdb9b
Oasis <71035180+oasis-gardens@users.noreply.github.com>
2021-07-08 09:59:04 -0400

    Update setup_bootloader.sh

--- scripts/setup_bootloader.sh
+++ scripts/setup_bootloader.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 sudo chmod +w /etc/rc.local
-sudo sed -ie "/^fi/a python3 /home/pi/oasis-grow/controller.py &" /etc/rc.local
+sudo sed -ie "/^fi/a python3.9 /home/pi/oasis-grow/controller.py &" /etc/rc.local
