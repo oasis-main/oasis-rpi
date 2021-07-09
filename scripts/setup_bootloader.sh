6f670e6c10362c94de201b0b489533f801521c19
Oasis <71035180+oasis-gardens@users.noreply.github.com>
2021-07-09 17:36:42 -0400

    call python3 like a regular person

--- scripts/setup_bootloader.sh
+++ scripts/setup_bootloader.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo chmod +w /etc/rc.local
-sudo sed -ie "/^fi/a python3.8 /home/pi/oasis-grow/controller.py &" /etc/rc.local
+sudo sed -ie "/^fi/a python3 /home/pi/oasis-grow/controller.py &" /etc/rc.local
