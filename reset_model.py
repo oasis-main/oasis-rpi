41b2dea5fdedeac49a7cb61e4de3ed1f43e3a70e
Oasis <71035180+oasis-gardens@users.noreply.github.com>
2021-07-10 11:09:26 -0400

    Update reset_model.py

--- reset_model.py
+++ reset_model.py
@@ -3,7 +3,13 @@ import json
 from subprocess import Popen
 
 #set proper path for modules
-sys.path.append("home/pi/.local/lib/python3.9/site-packages")
+sys.path.append('/home/pi/oasis-grow')
+sys.path.append('/usr/lib/python37.zip')
+sys.path.append('/usr/lib/python3.7')
+sys.path.append('/usr/lib/python3.7/lib-dynload')
+sys.path.append('/home/pi/.local/lib/python3.7/site-packages')
+sys.path.append('/usr/local/lib/python3.7/dist-packages')
+sys.path.append('/usr/lib/python3/dist-packages')
 
 #all functions depend on subprocess module
 def reset_device_state():
