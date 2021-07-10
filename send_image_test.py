cf9bd82097b6b2e824fe5d3a866a7620b237e75a
Oasis <71035180+oasis-gardens@users.noreply.github.com>
2021-07-10 11:09:51 -0400

    Update send_image_test.py

--- send_image_test.py
+++ send_image_test.py
@@ -2,7 +2,13 @@ import sys
 import cameraElement as cam
 
 #set proper path for modules
-sys.path.append("home/pi/.local/lib/python3.9/site-packages")
+sys.path.append('/home/pi/oasis-grow')
+sys.path.append('/usr/lib/python37.zip')
+sys.path.append('/usr/lib/python3.7')
+sys.path.append('/usr/lib/python3.7/lib-dynload')
+sys.path.append('/home/pi/.local/lib/python3.7/site-packages')
+sys.path.append('/usr/local/lib/python3.7/dist-packages')
+sys.path.append('/usr/lib/python3/dist-packages')
 
 cam.load_state()
 user, db, storage = cam.initialize_user(cam.access_config["refresh_token"])
