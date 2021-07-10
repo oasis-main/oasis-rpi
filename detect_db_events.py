a191f9f0ebeb355953375b3cf870fed5a9f09ca2
Oasis <71035180+oasis-gardens@users.noreply.github.com>
2021-07-10 10:19:14 -0400

    Update detect_db_events.py

--- detect_db_events.py
+++ detect_db_events.py
@@ -23,7 +23,13 @@ import sys
 import time
 
 #set proper path for modules
-sys.path.append("home/pi/.local/lib/python3.9/site-packages")
+sys.path.append('/home/pi/grow-ctrl')
+sys.path.append('/usr/lib/python37.zip')
+sys.path.append('/usr/lib/python3.7')
+sys.path.append('/usr/lib/python3.7/lib-dynload')
+sys.path.append('/home/pi/.local/lib/python3.7/site-packages')
+sys.path.append('/usr/local/lib/python3.7/dist-packages')
+sys.path.append('/usr/lib/python3/dist-packages')
 
 import signal
 import pyrebase
