ae084688bd3b2de91802c34f53bf6848d7504cc0
Owen Leonard <owenleonard11@gmail.com>
2021-07-19 12:43:34 -0400

    Add first package validation for testing.

--- scripts/validate_install.sh
+++ scripts/validate_install.sh
@@ -1,7 +1,10 @@
 #/bin/sh -e
 
 echo "Validating python packages..."
-
+if ! [ -d "/usr/local/lib/python3.9/site-packages/requests" ]; then
+    echo "package 'requests' not found"
+    exit 1
+fi
 
 echo "Checking network connectivity..."
 wget -q --spider http://google.com
@@ -9,5 +12,6 @@ wget -q --spider http://google.com
 if [ $? -eq 0 ]; then
     echo "Online"
 else
-    echo "Offline"
+    echo "Network is offline"
+    exit 2
 fi
