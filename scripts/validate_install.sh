4b41eded3f4e3777bceb11bb7308b6d6f8fe2a30
Owen Leonard <owenleonard11@gmail.com>
2021-07-21 10:51:01 -0400

    Add all packages to install validtaion

--- scripts/validate_install.sh
+++ scripts/validate_install.sh
@@ -1,10 +1,17 @@
 #/bin/sh -e
 
 echo "Validating python packages..."
-if ! [ -d "/usr/local/lib/python3.9/site-packages/requests" ]; then
-    echo "package 'requests' not found"
+if ! [ -d "/usr/local/lib/python3.9/site-packages/streamlit" ]; then
+    echo "'streamlit' not found, python3.9 install failed"
     exit 1
 fi
+
+for packageName in setuptools Cython numpy pandas firebase pyrebase python_jwt gcloud sseclient requests requests_toolbelt pickle5 serial PIL; do
+    if ! [ -d "usr/local/lib/python3.9/site-packages/$packageName"]; then
+        echo "'$packageName' not found"
+        exit 1
+    fi
+done
 
 echo "Checking network connectivity..."
 wget -q --spider http://google.com
