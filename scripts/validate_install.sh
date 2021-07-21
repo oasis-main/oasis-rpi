a4c47fc7ba4dc5fbde6db07d466082eed1b44464
Owen Leonard <owenleonard11@gmail.com>
2021-07-21 11:04:28 -0400

    Remove exit commands from install validation

--- scripts/validate_install.sh
+++ scripts/validate_install.sh
@@ -3,13 +3,11 @@
 echo "Validating python packages..."
 if ! [ -d "/usr/local/lib/python3.9/site-packages/streamlit" ]; then
     echo "'streamlit' not found, python3.9 install failed"
-    exit 1
 fi
 
 for packageName in setuptools Cython numpy pandas firebase pyrebase python_jwt gcloud sseclient requests requests_toolbelt pickle5 serial PIL; do
-    if ! [ -d "usr/local/lib/python3.9/site-packages/$packageName"]; then
+    if ! [ -d "usr/local/lib/python3.9/site-packages/$packageName" ]; then
         echo "'$packageName' not found"
-        exit 1
     fi
 done
 
@@ -20,5 +18,4 @@ if [ $? -eq 0 ]; then
     echo "Online"
 else
     echo "Network is offline"
-    exit 2
 fi
