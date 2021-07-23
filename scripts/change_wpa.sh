0e2de39c968fe1c6573bddcb1e3449e37a485e5d
Owen Leonard <owenleonard11@gmail.com>
2021-07-23 10:12:01 -0400

    Add logic to pwd change script

--- scripts/change_wpa.sh
+++ scripts/change_wpa.sh
@@ -1,2 +1,15 @@
-#/bin/sh -e
+#/bin/bash -e
 
+read -sp "Enter old password: " oldpwd
+pwdline=grep wpa_passphrase /etc/hostapd/hostapd.conf
+
+if [ "wpa_passphrase=$oldpwd" = "$pwdline" ]; then
+    read -sp "Enter new password: " newpwd
+    read -sp "Confirm new password: " newpwdconf
+
+    if [ "$newpwd" = "$newpwdconf" ]; then
+        sudo sed -i "s/$oldpwd/$newpwd" /etc/hostapd/hostapd.conf
+    fi
+else
+    echo "Incorrect password."
+fi
