48f084464ab1135e5c4ae0a1c045a5b6db01f02e
Owen Leonard <owenleonard11@gmail.com>
2021-07-23 10:33:36 -0400

    Add newlines to pwd confirmation

--- scripts/change_wpa.sh
+++ scripts/change_wpa.sh
@@ -4,8 +4,8 @@ read -sp "Enter old password: " oldpwd
 pwdline=`grep wpa_passphrase /etc/hostapd/hostapd.conf`
 
 if [ "wpa_passphrase=$oldpwd" = "$pwdline" ]; then
-    read -sp "Enter new password: " newpwd
-    read -sp "Confirm new password: " newpwdconf
+    read -sp "\nEnter new password: " newpwd
+    read -sp "\nConfirm new password: " newpwdconf
 
     if [ "$newpwd" = "$newpwdconf" ]; then
         sudo sed -i "s/$oldpwd/$newpwd/" /etc/hostapd/hostapd.conf
