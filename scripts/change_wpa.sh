3615dcbbb25dce6fe8cd7846b5827980dcc9dde9
Owen Leonard <owenleonard11@gmail.com>
2021-07-23 10:28:16 -0400

    Add backticks to grep call

--- scripts/change_wpa.sh
+++ scripts/change_wpa.sh
@@ -1,7 +1,7 @@
 #/bin/bash -e
 
 read -sp "Enter old password: " oldpwd
-pwdline=grep wpa_passphrase /etc/hostapd/hostapd.conf
+pwdline=`grep wpa_passphrase /etc/hostapd/hostapd.conf`
 
 if [ "wpa_passphrase=$oldpwd" = "$pwdline" ]; then
     read -sp "Enter new password: " newpwd
