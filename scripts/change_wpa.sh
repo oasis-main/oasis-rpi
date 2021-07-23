c65b7f5f0c3b755ed6fe2f68b69fcff61215bce4
Owen Leonard <owenleonard11@gmail.com>
2021-07-23 10:36:37 -0400

    Reformat newlines

--- scripts/change_wpa.sh
+++ scripts/change_wpa.sh
@@ -1,15 +1,22 @@
 #/bin/bash -e
 
 read -sp "Enter old password: " oldpwd
+printf "\n"
 pwdline=`grep wpa_passphrase /etc/hostapd/hostapd.conf`
 
 if [ "wpa_passphrase=$oldpwd" = "$pwdline" ]; then
-    read -sp "\nEnter new password: " newpwd
-    read -sp "\nConfirm new password: " newpwdconf
+    read -sp "Enter new password: " newpwd
+    printf "\n"
+    read -sp "Confirm new password: " newpwdconf
+    printf "\n"
 
     if [ "$newpwd" = "$newpwdconf" ]; then
         sudo sed -i "s/$oldpwd/$newpwd/" /etc/hostapd/hostapd.conf
+    else
+        echo "Passwords do not match."
+        printf "\n"
     fi
 else
     echo "Incorrect password."
+    printf "\n"
 fi
