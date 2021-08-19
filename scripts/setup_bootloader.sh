ae58881cf8630c3bd86ec22d4e5f759271041577
Mike <mike@oasisregenerative.com>
2021-08-19 16:04:49 -0400

    moved files into new directory tree, still need to update paths inside program

--- scripts/setup_bootloader.sh
+++ scripts/setup_bootloader.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 sudo chmod +w /etc/rc.local
-sudo sed -ie "/^fi/a python3 /home/pi/oasis-grow/controller.py &" /etc/rc.local
+sudo sed -ie "/^fi/a python3 /home/pi/oasis-grow/main.py &" /etc/rc.local
