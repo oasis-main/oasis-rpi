f6c92e6dd394c2502ad3ae6ac8923e33e66728ba
Oasis <71035180+oasis-gardens@users.noreply.github.com>
2021-08-23 18:15:07 -0400

    Update setup_config.sh

--- scripts/setup_config.sh
+++ scripts/setup_config.sh
@@ -17,11 +17,15 @@ cp /home/pi/oasis-grow/defaults/hardware_config_default_template.json /home/pi/o
 cp /home/pi/oasis-grow/defaults/access_config_default_template.json /home/pi/oasis-grow/configs/access_config.json
 cp /home/pi/oasis-grow/defaults/feature_toggles_default_template.json /home/pi/oasis-grow/configs/feature_toggles.json
 
+echo "Moving state files..."
+
 cp /home/pi/oasis-grow/defaults/device_state_default_template.json /home/pi/oasis-grow/state/device_state.json
+cp /home/pi/oasis-grow/defaults/device_state_default_template.json /home/pi/oasis-grow/state/concurrency_buffers/device_state_main.json
 cp /home/pi/oasis-grow/defaults/device_state_default_template.json /home/pi/oasis-grow/state/concurrency_buffers/device_state_grow_ctrl.json
 cp /home/pi/oasis-grow/defaults/device_state_default_template.json /home/pi/oasis-grow/state/concurrency_buffers/device_state_listener.json
 
 cp /home/pi/oasis-grow/defaults/grow_params_default_template.json /home/pi/oasis-grow/state/grow_params.json
+cp /home/pi/oasis-grow/defaults/grow_params_default_template.json /home/pi/oasis-grow/state/concurrency_buffers/grow_params_main.json
 cp /home/pi/oasis-grow/defaults/grow_params_default_template.json /home/pi/oasis-grow/state/concurrency_buffers/grow_params_grow_ctrl.json
 cp /home/pi/oasis-grow/defaults/grow_params_default_template.json /home/pi/oasis-grow/state/concurrency_buffers/grow_params_listener.json
 
