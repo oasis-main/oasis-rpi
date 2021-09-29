b570246bd83f9a67cfa3b39f50ec4f1bf5cf0d2d
Oasis <71035180+oasis-gardens@users.noreply.github.com>
2021-09-29 10:01:20 -0400

    Update oasis_setup.py

--- networking/oasis_setup.py
+++ networking/oasis_setup.py
@@ -25,6 +25,38 @@ import reset_model
 #create a password-protected lan interface for accepting credentials
 import streamlit as st
 
+#declare state variables
+device_state = None #describes the current state of the system
+grow_params = None #describes the grow configuration of the system
+hardware_config = None #holds hardware I/O setting & pin #s
+access_config = None #contains credentials for connecting to firebase
+feature_toggles = None #tells the system which features are in use
+
+def load_state(loop_limit=100000): #Depends on: 'json'; Modifies: device_state,hardware_config ,access_config
+    global device_state, feature_toggles, access_config, grow_params, hardware config
+
+    for i in list(range(int(loop_limit))): #try to load, check if available, make unavailable if so, write state if so, write availabke iff so,  
+        try:
+            with open("/home/pi/oasis-grow/configs/device_state.json") as d:
+                device_state = json.load(d) #get device state    
+                
+            with open("/home/pi/oasis-grow/configs/grow_params.json") as g:
+                grow_params = json.load(g) #get grow params   
+                
+            with open("/home/pi/oasis-grow/configs/access_config.json") as a:
+                access_config = json.load(a) #get access state
+                
+            with open ("/home/pi/oasis-grow/configs/feature_toggles.json") as f:
+                feature_toggles = json.load(f) #get feature toggles
+        
+            with open ("/home/pi/oasis-grow/configs/hardware_config.json") as h:
+                hardware_config = json.load(h) #get hardware config
+        
+            break
+            
+        except Exception as e:
+            print("Error occured while listener reading. Retrying...")
+
 #update wpa_supplicant.conf
 def modWiFiConfig(SSID, password):
     config_lines = [
@@ -66,29 +98,6 @@ def modAccessConfig(name, e, p):
         a.truncate()
 
     print("Access configs added")
-
-#checks first to see that main is not writing, if json valid, loads state
-def load_state(loop_limit=100000): #Depends on: 'json'; Modifies: device_state,hardware_config ,access_config
-    global device_state, feature_toggles, access_config, grow_params
-
-    for i in list(range(int(loop_limit))): #try to load, check if available, make unavailable if so, write state if so, write availabke iff so,  
-        try:
-            with open("/home/pi/oasis-grow/configs/device_state.json") as d:
-                device_state = json.load(d) #get device state
-                
-            with open("/home/pi/oasis-grow/configs/grow_params.json") as g:
-                grow_params = json.load(g) #get grow params
-
-            with open("/home/pi/oasis-grow/configs/access_config.json") as a:
-                access_config = json.load(a) #get access state
-
-            with open ("/home/pi/oasis-grow/configs/feature_toggles.json") as f:
-                feature_toggles = json.load(f) #get feature toggles
-
-            break
-        
-        except Exception as e:
-            print("Error occured while grow_ctrl.py reading. Retrying...")
 
 #save key values to .json
 def write_state(path,field,value, loop_limit=100000): #Depends on: load_state(), patch_firebase, 'json'; Modifies: path
