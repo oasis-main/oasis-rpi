835d3f373fcf6dc5416b112d538e2ff6e9005e65
Oasis <71035180+oasis-gardens@users.noreply.github.com>
2021-09-29 09:55:24 -0400

    Update detect_db_events.py

--- networking/detect_db_events.py
+++ networking/detect_db_events.py
@@ -36,37 +36,40 @@ import pyrebase
 from multiprocessing import Process, Queue
 import json
 
-#empty variables for state management
-device_state = None
-grow_params = None
-hardware_config = None
-access_config = None
-
 #declare listener list
 listener_list = []
 
-#checks first to see that main is not writing, if json valid, loads state
+#declare state variablesx
+device_state = None #describes the current state of the system
+grow_params = None #describes the grow configuration of the system
+hardware_config = None #holds hardware I/O setting & pin #s
+access_config = None #contains credentials for connecting to firebase
+feature_toggles = None #tells the system which features are in use
+
 def load_state(loop_limit=100000): #Depends on: 'json'; Modifies: device_state,hardware_config ,access_config
-    global device_state, feature_toggles, access_config, grow_params
+    global device_state, feature_toggles, access_config, grow_params, hardware config
 
     for i in list(range(int(loop_limit))): #try to load, check if available, make unavailable if so, write state if so, write availabke iff so,  
         try:
             with open("/home/pi/oasis-grow/configs/device_state.json") as d:
-                device_state = json.load(d) #get device state
+                device_state = json.load(d) #get device state    
                 
             with open("/home/pi/oasis-grow/configs/grow_params.json") as g:
-                grow_params = json.load(g) #get grow params
-
+                grow_params = json.load(g) #get grow params   
+                
             with open("/home/pi/oasis-grow/configs/access_config.json") as a:
                 access_config = json.load(a) #get access state
-
+                
             with open ("/home/pi/oasis-grow/configs/feature_toggles.json") as f:
                 feature_toggles = json.load(f) #get feature toggles
-
-            break
         
+            with open ("/home/pi/oasis-grow/configs/hardware_config.json") as h:
+                hardware_config = json.load(h) #get hardware config
+        
+            break
+            
         except Exception as e:
-            print("Error occured while listener reading. Retrying...")
+            print("Error occured while main.py reading. Retrying...")
 
 #save key values to .json
 def write_state(path,field,value, loop_limit=100000): #Depends on: load_state(), patch_firebase, 'json'; Modifies: path
