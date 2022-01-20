666a0e95cfa3f4118949afa973fdaa6e316026fe
Oasis-X <71035180+oasis-main@users.noreply.github.com>
2022-01-20 11:25:33 -0500

    Update setup_config.sh

--- scripts/setup_config.sh
+++ scripts/setup_config.sh
@@ -1,21 +1,21 @@
 #!/bin/sh -e
 
 echo "Creating local directories..."
-sudo mkdir /home/pi/oasis-grow/configs
-sudo mkdir /home/pi/oasis-grow/data_out
-sudo mkdir /home/pi/oasis-grow/data_out/image_feed
-sudo mkdir /home/pi/oasis-grow/data_out/sensor_feed
-sudo mkdir /home/pi/oasis-grow/data_out/logs
+mkdir /home/pi/oasis-grow/configs
+mkdir /home/pi/oasis-grow/data_out
+mkdir /home/pi/oasis-grow/data_out/image_feed
+mkdir /home/pi/oasis-grow/data_out/sensor_feed
+mkdir /home/pi/oasis-grow/data_out/logs
 
 echo "Moving configuration files..."
-sudo cp /home/pi/oasis-grow/defaults/hardware_config_default_template.json /home/pi/oasis-grow/configs/hardware_config.json
-sudo cp /home/pi/oasis-grow/defaults/access_config_default_template.json /home/pi/oasis-grow/configs/access_config.json
-sudo cp /home/pi/oasis-grow/defaults/feature_toggles_default_template.json /home/pi/oasis-grow/configs/feature_toggles.json
-sudo cp /home/pi/oasis-grow/defaults/device_state_default_template.json /home/pi/oasis-grow/configs/device_state.json
-sudo cp /home/pi/oasis-grow/defaults/grow_params_default_template.json /home/pi/oasis-grow/configs/grow_params.json
+cp /home/pi/oasis-grow/defaults/hardware_config_default_template.json /home/pi/oasis-grow/configs/hardware_config.json
+cp /home/pi/oasis-grow/defaults/access_config_default_template.json /home/pi/oasis-grow/configs/access_config.json
+cp /home/pi/oasis-grow/defaults/feature_toggles_default_template.json /home/pi/oasis-grow/configs/feature_toggles.json
+cp /home/pi/oasis-grow/defaults/device_state_default_template.json /home/pi/oasis-grow/configs/device_state.json
+cp /home/pi/oasis-grow/defaults/grow_params_default_template.json /home/pi/oasis-grow/configs/grow_params.json
 
 echo "Creating new lock_file..."
-sudo cp /home/pi/oasis-grow/defaults/locks_default_template.json /home/pi/oasis-grow/configs/locks.json
+cp /home/pi/oasis-grow/defaults/locks_default_template.json /home/pi/oasis-grow/configs/locks.json
 
 echo "Creating placeholder image..."
-sudo cp /home/pi/oasis-grow/defaults/default_placeholder_image.jpg /home/pi/image.jpg
+cp /home/pi/oasis-grow/defaults/default_placeholder_image.jpg /home/pi/image.jpg
