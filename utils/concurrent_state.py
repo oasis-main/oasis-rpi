5644bbc4b67e1d75ed9f8513b0a232de868bc4f2
Mike <leemichael289@gmail.com>
2022-09-15 21:25:20 -0400

    debugging state machine

--- utils/concurrent_state.py
+++ utils/concurrent_state.py
@@ -50,28 +50,28 @@ locks = None
 def load_state(loop_limit=1000): #Depends on: 'json'; Modifies: device_state,hardware_config ,access_config
     global structs
 
-    for name,struct in structs.items(): #now we're going to load an populat the data    
+    for struct in structs: #now we're going to load an populat the data    
 
         #load device state
         for i in list(range(int(loop_limit))): #try to load, pass and try again if fails
             try:
-                if name != 'sensor_info':
-                    config_filepath = "/home/pi/oasis-grow/configs/" + name + ".json"
+                if struct != 'sensor_info':
+                    config_filepath = "/home/pi/oasis-grow/configs/" + struct + ".json"
                 else:
-                    config_filepath = "/home/pi/oasis-grow/data_out/" + name + ".json"
+                    config_filepath = "/home/pi/oasis-grow/data_out/" + struct + ".json"
 
                 if not os.path.exists(config_filepath):
                     print(config_filepath + " does not exist. Have you run the setup scripts?")
                     return
 
                 with open(config_filepath, "rb") as x: #open the config filepath with bytes
-                        struct = orjson.loads(x.read()) #try to parse bytes to json -> dict
-                        print(struct)
+                        structs[struct] = orjson.loads(x.read()) #try to parse bytes to json -> dict
+                        #print(struct)
 
                 for k,v in struct.items(): 
                     if struct[k] is None:
-                        print("Read NoneType in " + name + "!")
-                        print("Resetting " + name + "...") 
+                        print("Read NoneType in " + struct + "!")
+                        print("Resetting " + struct + "...") 
                         reset_model.reset_config_path(config_filepath)
                     
                     else: 
@@ -82,9 +82,9 @@ def load_state(loop_limit=1000): #Depends on: 'json'; Modifies: device_state,har
             except Exception as e:
                 if i >= int(loop_limit):
                     reset_model.reset_device_state()
-                    print("Tried to read " + name + " max # of times. File is corrupted, resetting...")
+                    print("Tried to read " + struct + " max # of times. File is corrupted, resetting...")
                 else:
-                    print("Tried to read " + name + "while being written. If this continues, file is corrupt.")
+                    print("Tried to read " + struct + "while being written. If this continues, file is corrupt.")
                     pass
 
 #gets the mutex
@@ -125,9 +125,9 @@ def load_locks(loop_limit = 10000): #leave this alone since it's the python brid
 #save key values to .json
 def write_state(path, field, value, db_writer, loop_limit=1000): #Depends on: load_state(), 'json'; 
     if db_writer is not None: #Accepts(path, field, value, custom timeout, db_writer function); Modifies: path
-        if device_state["connected"] == "1": #write state to cloud
+        if structs["device_state"]["connected"] == "1": #write state to cloud
             try:
-                db_writer(access_config,field,value) #will be loaded in by listener, so is best represent change db first
+                db_writer(structs["access_config"],field,value) #will be loaded in by listener, so is best represent change db first
             except Exception as e:
                 print(err.fullstack())
                 pass
@@ -213,9 +213,9 @@ def write_dict(path, dictionary, db_writer, loop_limit=1000): #Depends on: load_
 
     if db_writer is not None:
         #these will be loaded in by the listener, so best to make sure we represent the change in firebase too
-        if device_state["connected"] == "1": #write state to cloud
+        if structs["device_state"]["connected"] == "1": #write state to cloud
             try:
-                db_writer(access_config, dictionary)
+                db_writer(structs["access_config"], dictionary)
             except Exception:
                 print(err.full_stack())
                 pass
@@ -297,7 +297,7 @@ def write_dict(path, dictionary, db_writer, loop_limit=1000): #Depends on: load_
 #Higher-order device_state checker
 def check(state, function, alt_function = None):
     load_state()
-    if device_state[state] == "1":
+    if structs["device_state"][state] == "1":
         function()
     else:
         if alt_function is not None:
