8d5e71c4cdaf1189f52e9d7992ef3f6cc9085444
Mike <leemichael289@gmail.com>
2022-09-28 17:03:38 -0400

    debounce was deprecated

--- rusty_pins/src/lib.rs
+++ rusty_pins/src/lib.rs
@@ -16,22 +16,22 @@ struct RelayOutput {
 #[pymethods] 
 impl ButtonInput { 
     #[new]
-    fn new(pin: i32) -> Self { //this is like __init__()
+    fn new(pin: u8) -> Self { //this is like __init__()
         ButtonInput {buttn: Button::new(pin)
          // Adds debouncing so that subsequent presses within 100ms don't trigger a press
         }
     }
 
     fn is_pressed(&self){
-        self.buttn.value()
+        self.buttn.value();
     }
 
     fn await_press(&mut self){
-        self.buttn.wait_for_press(None)
+        self.buttn.wait_for_press(None);
     }
 
     fn close(self){
-        self.buttn.close()
+        self.buttn.close();
     }
 
 }
