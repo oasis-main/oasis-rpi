6a0af32313f4e0c3bc7f15eab5f0ceb47c7f18eb
Mike <leemichael289@gmail.com>
2022-09-28 16:45:53 -0400

    implemented button with debouncing and await press, downgraded to maturin 16.4

--- rusty_pins/src/lib.rs
+++ rusty_pins/src/lib.rs
@@ -1,18 +1,7 @@
 use pyo3::prelude::*;
 use rust_gpiozero::{Button, Debounce};
 
-/*
-Relay code in python: example
-    #setup GPIO
-    
-    GPIO.setmode(GPIO.BCM) #GPIO Numbers instead of board numbers
-    water_GPIO = cs.structs["hardware_config"]["equipment_gpio_map"]["water_relay"] #heater pin pulls from config file
-    GPIO.setup(water_GPIO, GPIO.OUT) #GPIO setup
-    GPIO.output(water_GPIO, GPIO.LOW) #NO relay close = GPIO.HIGH, NO relay open = GPIO.LOW
-    GPIO.output(water_GPIO, GPIO.HIGH) #NC relay close = GPIO.LOW, NC relay open = GPIO.HIGH
- */
-
-// A "button" struct
+// A wrapper "button" struct
 #[pyclass]
 struct ButtonInput {
     buttn: Button
