60e24c7b6aa45e3e9e04e0ab5c2167220df6ec23
Mike <leemichael289@gmail.com>
2022-10-02 09:00:36 -0400

    testing rppal GPIO zero management library

--- rusty_pins/src/lib.rs
+++ rusty_pins/src/lib.rs
@@ -1,37 +1,42 @@
 use pyo3::prelude::*;
-use rust_gpiozero::{Button, OutputDevice};
+
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+
+use rppal::gpio::Gpio;
 
 // A wrapper "button" struct
 #[pyclass]
-struct ButtonInput {
-    buttn: Button
-}
-
+struct GPIO_out {input: Gpio}
 
 #[pymethods] 
-impl ButtonInput { 
+impl GPIO_out { 
     #[new]
     fn new(pin: u8) -> Self { //this is like __init__()
-        ButtonInput {buttn: Button::new(pin)
-        }
+        Gpio::new()?.get(pin)?.into_output()
+    }
+    
+    fn set_high(&mut self){
+        self.set_high();
     }
 
-    fn is_pressed(&self){
-        self.buttn.value();
-    }
-
-    fn await_press(&mut self){
-        self.buttn.wait_for_press(None);
-    }
-
-    fn close(&self){
-        self.buttn.close();
+    fn await_signal(&mut self){
+        self.set_low();
     }
 
 }
+
+//fn is_active(&self){
+//    self.buttn.value();
+//}
+
+//fn await_signal(&mut self){
+//    self.buttn.wait_for_press(None);
+//}
 
 #[pymodule]
 fn rusty_pins(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_class::<ButtonInput>()?;
+    m.add_class::<GPIO_out>()?;
     Ok(())
 }
