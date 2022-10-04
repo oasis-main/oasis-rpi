326c34064b66b74d095b6d492991fe510ef6de67
Mike <leemichael289@gmail.com>
2022-10-04 09:41:25 -0400

    added input pin

--- rusty_pins/src/lib.rs
+++ rusty_pins/src/lib.rs
@@ -2,40 +2,57 @@
 use pyo3::prelude::*;
 use rppal::gpio::Gpio;
 use rppal::gpio::OutputPin;
+use rppal::gpio::InputPin;
 
 // A Python-ready GPIO output class
 #[pyclass(unsendable)]
-struct GpioOut{out: OutputPin}
+struct GpioOut{output: OutputPin}
 
 // Behavior of the GPIO output class
 #[pymethods]
 impl GpioOut { 
-    #[new]
-    fn new(pin: u8) -> Self { //this is like __init__()
+    #[new] // call this from python with rusty_pins.GpioOut(pin#)
+    fn new(pin: u8) -> Self { //this is like python __init__()
         let io_pin = Gpio::new().unwrap().get(pin).unwrap().into_output();
-        GpioOut{out: io_pin}
+        GpioOut{output: io_pin}
     }
     
     fn set_high(&mut self){
-        self.out.set_high();
+        self.output.set_high();
     }
 
     fn set_low(&mut self){
-        self.out.set_low();
+        self.output.set_low();
     }
 
 }
 
-//fn is_active(&self){
-//    self.buttn.value();
-//}
+// A Python-ready GPIO input class
+#[pyclass(unsendable)]
+struct GpioIn{input: InputPin}
 
-//fn await_signal(&mut self){
-//    self.buttn.wait_for_press(None);
-//}
+// Behavior of the GPIO input class
+#[pymethods]
+impl GpioIn { 
+    #[new] // call this from python with rusty_pins.GpioOut(pin#)
+    fn new(pin: u8) -> Self { //this is like python __init__()
+        let io_pin = Gpio::new().unwrap().get(pin).unwrap().into_input();
+        GpioIn{input: io_pin}
+    }
 
+    fn check_high(&self) -> bool {
+        self.input.is_high()
+    }
+
+    fn check_low(&self) -> bool {
+        self.input.is_high()
+    }
+}
+
+// A Python-ready GPIO input class
 #[pymodule]
 fn rusty_pins(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<GpioOut>()?;
+    m.add_class::<GpioIn>()?;
     Ok(())
 }
