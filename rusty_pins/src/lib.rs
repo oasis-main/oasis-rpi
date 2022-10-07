c53df46e55fe08dc484fe4409a5624bb7038af79
Mike <leemichael289@gmail.com>
2022-10-07 12:38:31 -0400

    save work, try to finish up the firmware first

--- rusty_pins/src/lib.rs
+++ rusty_pins/src/lib.rs
@@ -1,8 +1,7 @@
 //Other people's hard work
 use pyo3::prelude::*;
-use rppal_w_frontend::gpio::Gpio;
-use rppal_w_frontend::gpio::OutputPin;
-use rppal_w_frontend::gpio::InputPin;
+use rppal::gpio::Gpio;
+use rppal::gpio::OutputPin;
 
 // A safe, Python-ready GPIO output class that operates on the hardware level
 #[pyclass(unsendable)]
@@ -27,32 +26,9 @@ impl GpioOut {
 
 }
 
-// A Python-ready GPIO input class
-#[pyclass(unsendable)]
-struct GpioIn{input: InputPin}  
-
-// Behavior of the GPIO input class
-#[pymethods]
-impl GpioIn { 
-    #[new] // call this from python with rusty_pins.GpioOut(pin#)
-    fn new(pin: u8) -> Self { //this is like python __init__()
-        let io_pin = Gpio::new().unwrap().get(pin).unwrap().into_input_pulldown();
-        GpioIn{input: io_pin}
-    }
-
-    fn check_high(&self) -> bool {
-        self.input.is_high()
-    }
-
-    fn check_low(&self) -> bool {
-        self.input.is_low()
-    }
-}
-
 // A Python-ready GPIO I/O module
 #[pymodule]
 fn rusty_pins(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<GpioOut>()?;
-    m.add_class::<GpioIn>()?;
     Ok(())
 }
